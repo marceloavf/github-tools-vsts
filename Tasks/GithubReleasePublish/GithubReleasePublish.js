d5fa4b697e7c675f97a8f8ec89813e08d20f861b
Marcelo Augusto Vieira Formentão <marcelo@precisaosistemas.com.br>
2018-01-22 21:34:10 -0200

    fix(task-release): remove gulp tool and use demand agent
    - Include validation of manifestJson

--- Tasks/GithubReleasePublish/GithubReleasePublish.js
+++ Tasks/GithubReleasePublish/GithubReleasePublish.js
@@ -1,13 +1,9 @@
+const gulp = require('gulp');
 const release = require('gulp-github-release');
 const tl = require('vsts-task-lib/task');
 
-// Check if gulp exists and create the tool runner
-const gulp = tl.which('gulp', true);
-const gulpExec = tl.tool(gulp);
-
 // Endpoint
 const githubEndpoint = tl.getInput('githubEndpoint');
-const githubEndpointUrl = tl.getEndpointUrl(githubEndpoint);
 const githubEndpointToken = tl.getEndpointAuthorization(githubEndpoint);
 // Inputs
 const githubRepository = tl.getInput('githubRepository').split('/');
@@ -21,18 +17,18 @@ const githubReleasePrerelease = tl.getBoolInput('githubReleasePrerelease');
 const manifestJson = tl.getPathInput('manifestJson');
 const githubReleaseAsset = tl.getPathInput('githubReleaseAsset');
 
-gulpExec.task('release', function() {
-  gulpExec.src(githubReleaseAsset).pipe(
-    release({
-      token: githubEndpointToken, // or you can set an env var called GITHUB_TOKEN instead
-      owner: githubRepository[0], // if missing, it will be extracted from manifest (the repository.url field)
-      repo: githubRepository[1], // if missing, it will be extracted from manifest (the repository.url field)
-      tag: githubTag, // if missing, the version will be extracted from manifest and prepended by a 'v'
-      name: githubReleaseTitle, // if missing, it will be the same as the tag
-      notes: githubReleaseNotes, // if missing it will be left undefined
-      draft: githubReleaseDraft, // if missing it's false
-      prerelease: githubReleasePrerelease, // if missing it's false
-      manifest: require(manifestJson) // package.json from which default values will be extracted if they're missing
-    })
-  );
-});
+const isJson = (path) => /^(.*\.json$).*$/.test(path);
+
+gulp.src(githubReleaseAsset).pipe(
+  release({
+    token: githubEndpointToken, // or you can set an env var called GITHUB_TOKEN instead
+    owner: githubRepository[0], // if missing, it will be extracted from manifest (the repository.url field)
+    repo: githubRepository[1], // if missing, it will be extracted from manifest (the repository.url field)
+    tag: githubTag, // if missing, the version will be extracted from manifest and prepended by a 'v'
+    name: githubReleaseTitle, // if missing, it will be the same as the tag
+    notes: githubReleaseNotes, // if missing it will be left undefined
+    draft: githubReleaseDraft, // if missing it's false
+    prerelease: githubReleasePrerelease, // if missing it's false
+    manifest: isJson(manifestJson) ? require(manifestJson) : null // package.json from which default values will be extracted if they're missing
+  })
+);
