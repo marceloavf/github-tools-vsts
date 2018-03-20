2846e2167ff3381fce06047ea8c40913cc3123f3
Marcelo Augusto Vieira Formentão <marceloavf3@hotmail.com>
2018-03-20 09:00:25 -0300

    fix(bytestosize): remove extra parenthesis

--- Common/Node/bytesToSize.js
+++ Common/Node/bytesToSize.js
@@ -6,7 +6,7 @@ const bytesToSize = bytes => {
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
   if (bytes === 0) return 'n/a'
   const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10)
-  if (i === 0) return `${bytes} ${sizes[i]})`
+  if (i === 0) return `${bytes} ${sizes[i]}`
   return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`
 }
 
